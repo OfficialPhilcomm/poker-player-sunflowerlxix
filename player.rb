4a6f880f1ae078ae866b9d2699cade56959f2980
Philipp Schlesinger <info@philcomm.dev>
2022-08-24 11:06:24 +0200

    Add default bet action

--- player.rb
+++ player.rb
@@ -4,7 +4,8 @@ class Player
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
-    0
+    player_in_action = game_state["in_action"]
+    game_state["current_buy_in"] - game_state["players"][player_in_action]["bet"]
   end
 
   def showdown(game_state)
