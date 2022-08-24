b5c7a21aa4f9f16d327184542823ccb3a182ebc1
Philipp Schlesinger <info@philcomm.dev>
2022-08-24 11:29:14 +0200

    Fold if no card has value over 9

--- player.rb
+++ player.rb
@@ -4,8 +4,17 @@ class Player
   VERSION = "Very good version"
 
   def bet_request(game_state)
-    player_in_action = game_state["in_action"]
-    game_state["current_buy_in"] - game_state["players"][player_in_action]["bet"]
+    me = game_state["players"][game_state["in_action"]]
+
+    has_value_card = me["hole_cards"].select do |hole_card|
+      ["10", "J", "Q", "K", "A"].include? hole_card["rank"]
+    end.any?
+
+    if has_value_card
+      game_state["current_buy_in"] - me["bet"]
+    else
+      0
+    end
   end
 
   def showdown(game_state)
