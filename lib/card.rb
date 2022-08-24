f3c232f3bb0bf870a7d46625b8ce3593a6689b2c
Philipp Schlesinger <info@philcomm.dev>
2022-08-24 16:04:01 +0200

    Add value attr

--- lib/card.rb
+++ lib/card.rb
@@ -13,4 +13,22 @@ class Card
   def over_nine?
     %w[10 J Q K A].include? @rank
   end
+
+  def value
+    {
+      "2" => 2
+      "3" => 3
+      "4" => 4
+      "5" => 5
+      "6" => 6
+      "7" => 7
+      "8" => 8
+      "9" => 9
+      "10" => 10
+      "J" => 11
+      "Q" => 12
+      "K" => 13
+      "A" => 14
+    }
+  end
 end
