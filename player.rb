2a3033f65ca82ed43f3fccf09c633a6f0a4fbceb
Philipp Schlesinger <info@philcomm.dev>
2022-08-24 14:10:29 +0200

    Split methods

--- player.rb
+++ player.rb
@@ -7,6 +7,18 @@ class Player
   def bet_request(game_state)
     game_state = OpenStruct.new(game_state)
 
+    if game_state.community_cards
+      handle_first_round(game_state)
+    else
+      handle_other_rounds(game_state)
+    end
+  end
+
+  def showdown(game_state)
+
+  end
+
+  def handle_first_round(game_state)
     me = OpenStruct.new(game_state.players[game_state.in_action])
 
     has_value_card = me.hole_cards.select do |hole_card|
@@ -20,7 +32,17 @@ class Player
     end
   end
 
-  def showdown(game_state)
+  def handle_other_rounds(game_state)
+    me = OpenStruct.new(game_state.players[game_state.in_action])
 
+    has_value_card = me.hole_cards.select do |hole_card|
+      Card.from_json(hole_card).over_nine?
+    end.any?
+
+    if has_value_card
+      game_state.current_buy_in - me.bet
+    else
+      0
+    end
   end
 end
